a121250d366b85dc942c1898bee5a061f5b88240
RianAina <128467312+RianAina@users.noreply.github.com>
2023-12-04 22:51:27 +0300

    Created the CRUD about transaction

--- src/crudOperation/transactionCRUD.java
+++ src/crudOperation/transactionCRUD.java
@@ -1,1 +1,40 @@
+package crudOperation;
 
+import connection.DBConnection;
+import models.TransactionModel;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class transactionCRUD {
+
+    private Connection connection;
+
+    public TransactionCRUD() {
+        this.connection = DBConnection.get_connection();
+    }
+
+    public List<TransactionModel> findAll() {
+        String sql = "SELECT * FROM transaction;";
+        List<TransactionModel> resultList = new ArrayList<>();
+        try (Statement statement = connection.createStatement();
+             ResultSet result = statement.executeQuery(sql)) {
+
+            while (result.next()) {
+                resultList.add(new TransactionModel(
+                        result.getInt("id_transaction"),
+                        result.getString("transaction_description"),
+                        result.getInt("account_sending"),
+                        result.getDouble("debit"),
+                        result.getString("transaction_date")
+                ));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return resultList;
+    }
+
+}
