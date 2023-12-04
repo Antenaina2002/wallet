92260cc6b2b3cb646489bb0f3588fb1c49d67ca3
Antenaina2002 <hei.antenaina@gmail.com>
2023-12-04 22:30:41 +0100

    feat: implement currencyModel class. Represent currency entity data.

---
+++ src/models/currencyModel.java
@@ -0,0 +1,13 @@
+package models;
+
+public class currencyModel {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public currencyModel(String name) {
+        this.name = name;
+    }
+}
