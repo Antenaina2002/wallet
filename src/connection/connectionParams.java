a3c2ca66aae2c8b333d2f52c9cbc4e680135bc49
Antenaina2002 <hei.antenaina@gmail.com>
2023-12-04 18:37:53 +0100

    feat: created connection directory for dbConnection. Created connectionParams file and added database connection parameters.

---
+++ src/connection/connectionParams.java
@@ -0,0 +1,7 @@
+package connection;
+
+public class connectionParams {
+    public static final String URL = "jdbc:postgresql://localhost/library_management";
+    public static final String USER = "postgres";
+    public static final String PASSWORD = "your_password";
+}
