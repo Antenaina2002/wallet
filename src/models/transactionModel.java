99ada96afab96e3e9269cc12ec301e8384a554f0
RianAina <128467312+RianAina@users.noreply.github.com>
2023-12-04 22:58:19 +0300

    Fixed an error with dependancies

--- src/models/transactionModel.java
+++ src/models/transactionModel.java
@@ -1,12 +1,52 @@
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class transactionModel {
     private int id;
     private String type;
     private int accountId;
     private double amount;
     private String date;
+
+    // Getters and setters for id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getters and setters for type
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    // Getters and setters for accountId
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    // Getters and setters for amount
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    // Getters and setters for date
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
