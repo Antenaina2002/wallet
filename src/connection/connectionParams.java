37f46814c7d51b441801f953d5a40d2b878ac617
Antenaina2002 <hei.antenaina@gmail.com>
2023-12-04 19:29:58 +0100

    feat: added a class Connection that will do the database connection.

--- src/connection/connectionParams.java
+++ src/connection/connectionParams.java
@@ -1,7 +1,7 @@
 package connection;
 
 public class connectionParams {
-    public static final String URL = "jdbc:postgresql://localhost/";
-    public static final String USER = "postgres";
+    public static final String URL = "jdbc:postgresql://localhost/[database_name]";
+    public static final String USER = "your_username";
     public static final String PASSWORD = "your_password";
 }
