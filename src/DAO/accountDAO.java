fdad4eef9a579fc7b81e8898d34b93d705df0033
Antenaina2002 <hei.antenaina@gmail.com>
2023-12-07 23:02:02 +0300

    feat: implemented crudOperator and renamed to accountDAO

--- src/DAO/accountDAO.java
+++ src/DAO/accountDAO.java
@@ -7,7 +7,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AccountCRUD {
+public class AccountCRUD implements crudOperator<accountModel> {
 
     private Connection connection;
 
@@ -15,6 +15,7 @@ public class AccountCRUD {
         this.connection = dbConnection.get_connection();
     }
 
+    @Override
     public List<accountModel> findAll() {
         String sql = "SELECT * FROM account;";
         List<accountModel> resultList = new ArrayList<>();
@@ -36,6 +37,34 @@ public class AccountCRUD {
         return resultList;
     }
 
+    @Override
+    public List<accountModel> saveAll(List<accountModel> toSave) {
+        String sql = "INSERT INTO account (user, RIB, wallet) VALUES (?, ?, ?);";
+        try (PreparedStatement prepared = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+            for (accountModel account : toSave) {
+                prepared.setString(1, account.getUser());
+                prepared.setString(2, account.getRIB());
+                prepared.setInt(3, account.getWallet());
+                prepared.addBatch();
+            }
+
+            prepared.executeBatch();
+
+            ResultSet generatedKeys = prepared.getGeneratedKeys();
+            int i = 0;
+            while (generatedKeys.next()) {
+                toSave.get(i).setId(generatedKeys.getInt(1));
+                i++;
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return toSave;
+    }
+
+    @Override
     public accountModel save(accountModel toSave) {
         String sql = "INSERT INTO account (user, RIB, wallet) VALUES (?, ?, ?);";
         try (PreparedStatement prepared = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
@@ -57,16 +86,15 @@ public class AccountCRUD {
         return toSave;
     }
 
-    public void update(accountModel toUpdate) {
-        String sql = "UPDATE account SET user = ?, RIB = ?, wallet = ? WHERE id = ?;";
+    @Override
+    public accountModel delete(accountModel toDelete) {
+        String sql = "DELETE FROM account WHERE id = ?;";
         try (PreparedStatement prepared = connection.prepareStatement(sql)) {
-            prepared.setString(1, toUpdate.getUser());
-            prepared.setString(2, toUpdate.getRIB());
-            prepared.setInt(3, toUpdate.getWallet());
-            prepared.setInt(4, toUpdate.getId());
+            prepared.setInt(1, toDelete.getId());
             prepared.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        return toDelete;
     }
 }
---
+++ src/DAO/crudOperator.java
@@ -0,0 +1,10 @@
+package DAO;
+
+import java.util.List;
+
+public interface crudOperator<T> {
+    List<T> findAll();
+    List<T> saveAll(List<T> toSave);
+    T save(T toSave);
+    T delete(T toDelete);
+}
