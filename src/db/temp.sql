cef0c901c51bfd1b8c4b7cc932e5a0902feecaf0
RianAina <128467312+RianAina@users.noreply.github.com>
2023-12-15 01:20:26 +0300

    Added the function about transaction

--- src/db/temp.sql
+++ src/db/temp.sql
@@ -39,3 +39,41 @@ CREATE TABLE IF NOT EXISTS transaction (
     type_transaction VARCHAR(10) CHECK (type_transaction IN ('Débit', 'Crédit')),
     compte_id INT REFERENCES compte(id)
 );
+
+-- Table pour les catégories de transactions
+CREATE TABLE IF NOT EXISTS catégorie_transaction (
+    id SERIAL PRIMARY KEY,
+    référence VARCHAR(8) UNIQUE,
+    catégorie VARCHAR(50) UNIQUE,
+    type_transaction VARCHAR(10) CHECK (type_transaction IN ('Débit', 'Crédit'))
+);
+
+-- Insertion des catégories de transactions
+INSERT INTO catégorie_transaction (référence, catégorie, type_transaction) VALUES
+    (LPAD(FLOOR(RANDOM() * 100000000), 8, '0'), 'alimentation', 'Débit'),
+    (LPAD(FLOOR(RANDOM() * 100000000), 8, '0'), 'transport', 'Débit'),
+    (LPAD(FLOOR(RANDOM() * 100000000), 8, '0'), 'divertissement', 'Débit'),
+    (LPAD(FLOOR(RANDOM() * 100000000), 8, '0'), 'salaire', 'Crédit'),
+    (LPAD(FLOOR(RANDOM() * 100000000), 8, '0'), 'transfert_entrant', 'Crédit'),
+    (LPAD(FLOOR(RANDOM() * 100000000), 8, '0'), 'pret_debit', 'Débit'),
+    (LPAD(FLOOR(RANDOM() * 100000000), 8, '0'), 'pret_credit', 'Crédit')
+ON CONFLICT (catégorie) DO NOTHING;
+
+-- Fonction pour insérer une nouvelle transaction
+CREATE OR REPLACE FUNCTION fonction_transaction(catégorie VARCHAR, montant DECIMAL)
+RETURNS VOID AS $$
+DECLARE
+    référence_transaction VARCHAR(8);
+BEGIN
+    SELECT référence INTO référence_transaction
+    FROM catégorie_transaction
+    WHERE catégorie_transaction.catégorie = catégorie;
+
+    IF référence_transaction IS NOT NULL THEN
+        INSERT INTO transaction (label, montant, date_transaction, type_transaction, compte_id)
+        VALUES (catégorie, montant, CURRENT_TIMESTAMP, (SELECT type_transaction FROM catégorie_transaction WHERE catégorie = catégorie LIMIT 1), 1); -- Modifier le compte_id selon vos besoins
+    ELSE
+        RAISE EXCEPTION 'Catégorie de transaction non trouvée.';
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
