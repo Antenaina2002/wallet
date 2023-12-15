2ff9db0eb47c269117eb43da4b1cdee72eeefef7
Antenaina2002 <hei.antenaina@gmail.com>
2023-12-15 14:58:35 +0300

    feat: created model for historiqueSolde

--- src/models/historiqueSolde.java
+++ src/models/historiqueSolde.java
@@ -1,14 +1,60 @@
 package models;
 
 import java.sql.Timestamp;
-import java.util.List;
 
 public class historiqueSolde {
     private int id;
-    private String nom;  // Changer de "name" à "nom"
-    private double montantSolde;
-    private Timestamp dateDerniereMiseAJourSolde;
-    private List<transaction> transactions;
-    private String devise;
-    private String type;
+    private int compte; // Identifiant du compte lié
+    private double ancienSolde; // Solde avant la modification
+    private double nouveauSolde; // Solde après la modification
+    private Timestamp dateHistorique; // Date de la modification
+
+    public historiqueSolde(int id, int compte, double ancienSolde, double nouveauSolde, Timestamp dateHistorique) {
+        this.id = id;
+        this.compte = compte;
+        this.ancienSolde = ancienSolde;
+        this.nouveauSolde = nouveauSolde;
+        this.dateHistorique = dateHistorique;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCompte() {
+        return compte;
+    }
+
+    public double getAncienSolde() {
+        return ancienSolde;
+    }
+
+    public double getNouveauSolde() {
+        return nouveauSolde;
+    }
+
+    public Timestamp getDateHistorique() {
+        return dateHistorique;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCompte(int compte) {
+        this.compte = compte;
+    }
+
+    public void setAncienSolde(double ancienSolde) {
+        this.ancienSolde = ancienSolde;
+    }
+
+    public void setNouveauSolde(double nouveauSolde) {
+        this.nouveauSolde = nouveauSolde;
+    }
+
+    public void setDateHistorique(Timestamp dateHistorique) {
+        this.dateHistorique = dateHistorique;
+    }
+
 }
