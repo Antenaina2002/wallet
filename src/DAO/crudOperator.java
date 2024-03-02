950b6cf05627dc1c49099ed7541a6c0a8a95315d
Antenaina2002 <hei.antenaina@gmail.com>
2024-03-02 17:22:08 +0300

    initial commit wallet-spring

--- src/DAO/crudOperator.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-
-import java.util.List;
-
-public interface crudOperator<T> {
-    List<T> findAll();
-    List<T> saveAll(List<T> toSave);
-    T save(T toSave);
-}
--- src/connection/connectionParams.java
+++
@@ -1,7 +0,0 @@
-package connection;
-
-public class connectionParams {
-    public static final String URL = "jdbc:postgresql://localhost/[account_management]";
-    public static final String USER = "postgres";
-    public static final String PASSWORD = "        ";
-}
